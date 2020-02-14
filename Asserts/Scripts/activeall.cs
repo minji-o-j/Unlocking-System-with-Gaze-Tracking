2c682829a3bba8d9fc5bc663c2da667a11513ec2
Minji <jminji98@gmail.com>
2020-02-14 22:22:51 +0900

    active lemon
    Co-Authored-By: sohyeon98720 <sohyeon98720@users.noreply.github.com>
    Co-Authored-By: hyeinlee725 <hyeinlee725@users.noreply.github.com>

---
+++ Asserts/Scripts/activeall.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class activeall : MonoBehaviour {
+
+	float fTime=0f;
+
+	public GameObject _1;
+	public GameObject _2;
+	public GameObject _3;
+	public GameObject _4;
+	public GameObject _5;
+	public GameObject _6;
+	public GameObject _7;
+	public GameObject Text;
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        fTime += Time.deltaTime;
+        if (fTime >= 2)
+        {
+            Text.SetActive(false);
+            _1.SetActive(true);
+            _2.SetActive(true);
+            _3.SetActive(true);
+            _4.SetActive(true);
+            _5.SetActive(true);
+            _6.SetActive(true);
+            _7.SetActive(true);
+
+        //    SceneManager.LoadScene("Scene2-2");
+        }
+        string file = @"C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\password.txt";
+        if (File.Exists(file))
+        {
+            SceneManager.LoadScene("Scene2-2");
+        }
+    }
+}
