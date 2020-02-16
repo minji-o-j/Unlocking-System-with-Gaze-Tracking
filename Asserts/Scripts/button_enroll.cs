5ad0bf2b41923dd47dd7b87bf8d0eae9108cc1c9
Minji <jminji98@gmail.com>
2020-02-17 00:02:33 +0900

    enroll
    Co-Authored-By: sohyeon98720 <sohyeon98720@users.noreply.github.com>
    Co-Authored-By: hyeinlee725 <hyeinlee725@users.noreply.github.com>

---
+++ Asserts/Scripts/button_enroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button_enroll : MonoBehaviour {
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void changeScene()
+	{
+		SceneManager.LoadScene("Scene2");
+	}
+}
