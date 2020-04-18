30813ab2a087165c030ced9781fceaa0503a21e0
Minji <jminji98@gmail.com>
2020-04-19 03:42:11 +0900

    button_end
    Co-Authored-By: hyeinlee725 <hyeinlee725@users.noreply.github.com>
    Co-Authored-By: sohyeon98720 <sohyeon98720@users.noreply.github.com>

---
+++ Asserts/Scripts/button_end.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class button_end : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void toHone()
+    {
+        SceneManager.LoadScene("Scene1");//홈으로
+    }
+    public void goBack()
+    {
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\realTime2.csv");
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\realTime2.csv.meta");
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\correctPassword.txt");
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\correctPassword.txt.meta");
+
+        SceneManager.LoadScene("Scene3-2");//인증-비번입력부터
+        
+    }
+}
