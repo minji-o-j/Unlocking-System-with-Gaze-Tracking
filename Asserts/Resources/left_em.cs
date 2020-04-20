9f5e9516473a8237a009961eb38bb6ef97047c38
Minji <jminji98@gmail.com>
2020-04-21 01:14:26 +0900

    left
    Co-Authored-By: sohyeon98720 <sohyeon98720@users.noreply.github.com>
    Co-Authored-By: hyeinlee725 <hyeinlee725@users.noreply.github.com>

---
+++ Asserts/Resources/left_em.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class left_em : MonoBehaviour
+  
+{
+    public int Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localEulerAngles = new Vector3((float)1.922, (float)179.993, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.localEulerAngles = new Vector3((float)0.008, (float)179.993, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localEulerAngles = new Vector3((float)-29.437, (float)179.993, 0);
+        }
+    }
+}
