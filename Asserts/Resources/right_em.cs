c59629f969039cb23b7135472d833454a5fba116
Minji <jminji98@gmail.com>
2020-05-28 00:02:15 +0900

    Right
    Co-Authored-By: sohyeon98720 <sohyeon98720@users.noreply.github.com>
    Co-Authored-By: hyeinlee725 <hyeinlee725@users.noreply.github.com>

---
+++ Asserts/Resources/right_em.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class right_em : MonoBehaviour
+{
+    public int Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localEulerAngles=new Vector3((float)-30.696, (float)-0.007, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.localEulerAngles = new Vector3((float)0.022, (float)-0.007, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localEulerAngles = new Vector3((float)0.137, (float)-0.007, 0);
+        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Vector3 pos = Input.mousePosition;
+        //    pos.z = 10;
+        //    Vector3 target = Camera.main.ScreenToWorldPoint(pos);
+        //    print("target" + target);
+
+        //    transform.LookAt(target);
+    }
+    
+}
