0e62d01e86aa9e046a158e7ba1290b7f80d93572
minji-o-j <jminji98@gmail.com>
2020-07-29 00:13:36 +0900

    cancel
    Co-Authored-By: sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
    Co-Authored-By: hyeinlee725 <59090549+hyeinlee725@users.noreply.github.com>

---
+++ Asserts/Scripts/button_cancel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class button_cancel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\realTime.csv");
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\realTime.csv.meta");
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\password.txt");
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\password.txt.meta");
+        SceneManager.LoadScene("Scene1");
+    }
+}
