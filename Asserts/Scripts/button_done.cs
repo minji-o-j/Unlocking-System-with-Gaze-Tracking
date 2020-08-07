011bca66d0ecde1b304719c1e6af97e7e05e1d2b
minji-o-j <jminji98@gmail.com>
2020-08-08 00:24:20 +0900

    button
    Co-Authored-By: hyeinlee725 <59090549+hyeinlee725@users.noreply.github.com>
    Co-Authored-By: sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>

---
+++ Asserts/Scripts/button_done.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class button_done : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeScene()
+    {
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\realTime.csv");
+        File.Delete("C:\\Users\\minji\\Desktop\\unity_practice\\lenon test\\Assets\\Resources\\realTime.csv.meta");
+        SceneManager.LoadScene("Scene1");
+    }
+}
